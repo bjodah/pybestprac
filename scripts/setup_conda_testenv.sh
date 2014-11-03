4a780df4e943fdaa732657c31f8f2a542f28c57e
Björn Dahlgren <bjodah@gmail.com>
2014-11-03 22:33:30 +0100

    Use pip to install pycompilation and pycodeexport

--- scripts/setup_conda_testenv.sh
+++ scripts/setup_conda_testenv.sh
@@ -2,6 +2,6 @@
 
 PY_VERSION=$1
 ENV_NAME=$2
-conda create --quiet -n $ENV_NAME python=${PY_VERSION} cython=0.21 pip sphinx pytest numpydoc future pycompilation pycodeexport
+conda create --quiet -n $ENV_NAME python=${PY_VERSION} cython=0.21 pip sphinx pytest numpydoc future
 source activate $ENV_NAME
-pip install --quiet argh pytest-pep8 pytest-cov python-coveralls sphinx_rtd_theme https://github.com/sympy/sympy/archive/master.zip
+pip install --quiet argh pytest-pep8 pytest-cov python-coveralls sphinx_rtd_theme pycompilation pycodeexport https://github.com/sympy/sympy/archive/master.zip
