2dbd2698db27c8e055cba86fd2d7f8fb65e7eeec
Björn Dahlgren <bjodah@gmail.com>
2014-11-10 19:26:05 +0100

    Added debugging statements to run_test.py

--- conda-recipe/run_test.py
+++ conda-recipe/run_test.py
@@ -1,3 +1,20 @@
-import pybestprac
-# note that this is not the full test suite.
-assert abs(pybestprac.euclidean_norm([(1,)])[0] - 1) < 1e-15
+import sys
+import traceback
+
+print("Importing pybestprac...")
+try:
+    import pybestprac
+except ImportError:
+    print("Importing pybestprac failed")
+    traceback.print_exc(file=sys.stdout)
+
+print("Running tests...")
+try:
+    # note that this is not the full test suite.
+    assert abs(pybestprac.euclidean_norm([(1,)])[0] - 1) < 1e-15
+except AssertionError:
+    print("Assertion failed:")
+    traceback.print_exc(file=sys.stdout)
+else:
+    print("Other error:")
+    traceback.print_exc(file=sys.stdout)
