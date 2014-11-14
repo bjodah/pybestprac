16cd2b2132cc9143ca62345adc1c5e9debcc63ad
Björn Dahlgren <bjodah@gmail.com>
2014-11-14 18:43:31 +0100

    Adapt for travis/drone.

--- scripts/ci_conda.sh
+++ scripts/ci_conda.sh
@@ -4,6 +4,8 @@
 export PYTHON_VERSION=$1
 export CONDA_PY=$2
 export ENV_NAME=$3
+export RUN_TESTS=$4
+
 export CONDA_PATH=$(conda info --system | grep sys.prefix | cut -d: -f2 | sed -e 's/^ *//')
 source ./scripts/setup_conda_testenv.sh $PYTHON_VERSION $ENV_NAME
 conda info
@@ -12,4 +14,6 @@ python setup.py --version
 export DISTUTILS_DEBUG=1
 conda build conda-recipe
 conda install --quiet pybestprac --use-local
-LIBRARY_PATH=$CONDA_PATH/envs/$ENV_NAME/lib:$LIBRARY_PATH ./scripts/run_tests.sh
+if [[ "$RUN_TESTS" == "1" ]]; then
+    LIBRARY_PATH=$CONDA_PATH/envs/$ENV_NAME/lib:$LIBRARY_PATH ./scripts/run_tests.sh
+fi
